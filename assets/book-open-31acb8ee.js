40391b49cb41d631cd1a4ab822449343f78fed59
kRamu81 <kanamramu18@gmail.com>
2026-06-25 21:40:17 +0530

    Updates

--- assets/book-open-31acb8ee.js
+++
@@ -1,6 +0,0 @@
-import{ap as a}from"./index-7dcab68e.js";/**
- * @license lucide-react v1.21.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"M12 7v14",key:"1akyts"}],["path",{d:"M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z",key:"ruj8y"}]],t=a("book-open",o);export{t as B};
---
+++ assets/utils-c0879fe6.js
@@ -0,0 +1,1 @@
+import{s as o}from"./index-1bae781b.js";const e=o.useSyncExternalStore;function r(t,n){return typeof t=="function"?t(...n):!!t}export{r as s,e as u};
